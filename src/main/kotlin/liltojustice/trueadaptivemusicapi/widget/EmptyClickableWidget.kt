d73b7352d027d724ee131eabf2217994d4f65b57
LilTOJustice <muianick4@gmail.com>
2026-04-15 00:17:40 -0700

    Update 1.20.1

--- src/main/kotlin/liltojustice/trueadaptivemusicapi/widget/EmptyClickableWidget.kt
+++ src/main/kotlin/liltojustice/trueadaptivemusicapi/widget/EmptyClickableWidget.kt
@@ -1,15 +1,14 @@
 package liltojustice.trueadaptivemusicapi.widget
 
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder
 import net.minecraft.client.gui.widget.ClickableWidget
-import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.Text
 
 internal class EmptyClickableWidget: ClickableWidget(0, 0, 0, 0, Text.of("")) {
-    override fun render(matrices: MatrixStack?, mouseX: Int, mouseY: Int, delta: Float) {
-        super.render(matrices, mouseX, mouseY, delta)
+    override fun renderButton(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
     }
 
-    override fun appendNarrations(builder: NarrationMessageBuilder?) {
+    override fun appendClickableNarrations(builder: NarrationMessageBuilder?) {
     }
 }
