881ffa1ce8861c7f57a3b86404ca3e12141b5e72
LilTOJustice <muianick4@gmail.com>
2026-04-22 00:26:43 -0700

    1.1 complete

--- src/main/kotlin/liltojustice/trueadaptivemusicapi/trigger/predicate/type/BasicPredicateType.kt
+++ src/main/kotlin/liltojustice/trueadaptivemusicapi/trigger/predicate/type/BasicPredicateType.kt
@@ -1,17 +1,19 @@
 package liltojustice.trueadaptivemusicapi.trigger.predicate.type
 
 import liltojustice.trueadaptivemusicapi.trigger.arguments.EmptyTriggerArguments
+import kotlin.reflect.typeOf
 
 @Suppress("UNUSED")
-abstract class BasicPredicateType(typeName: String): StaticPredicateType<EmptyTriggerArguments>(typeName) {
+abstract class BasicPredicateType(typeName: String)
+    : StaticPredicateType<EmptyTriggerArguments>(typeName, typeOf<EmptyTriggerArguments>()) {
     final override val argDescriptions: Map<String, String>
         get() = super.argDescriptions
     final override val argDisplayNames: Map<String, String>
         get() = super.argDisplayNames
 
-    abstract fun validate(): Boolean
+    abstract fun test(): Boolean
 
-    final override fun validatePredicate(arguments: EmptyTriggerArguments): Boolean {
-        return validate()
+    final override fun test(arguments: EmptyTriggerArguments): Boolean {
+        return test()
     }
 }
