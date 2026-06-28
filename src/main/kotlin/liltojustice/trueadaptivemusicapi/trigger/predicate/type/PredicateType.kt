0158f91f300b80ac6e232cfcf37c660202d328c6
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-06-28 00:47:43 -0700

    TAM API 1.2.3 (#27) (#30)
    * 1.2.3 Version Bump
    
    * Add event and predicate argument prettification
    
    * Update CHANGELOG.md

--- src/main/kotlin/liltojustice/trueadaptivemusicapi/trigger/predicate/type/PredicateType.kt
+++ src/main/kotlin/liltojustice/trueadaptivemusicapi/trigger/predicate/type/PredicateType.kt
@@ -3,12 +3,18 @@ package liltojustice.trueadaptivemusicapi.trigger.predicate.type
 import liltojustice.trueadaptivemusicapi.trigger.DowncastTriggerType
 import liltojustice.trueadaptivemusicapi.trigger.arguments.TriggerArguments
 import liltojustice.trueadaptivemusicapi.trigger.state.TriggerState
+import liltojustice.trueadaptivemusicapi.util.StringExtensions.prettify
 import kotlin.reflect.KType
+import kotlin.reflect.full.declaredMembers
+import kotlin.reflect.jvm.jvmErasure
 
 abstract class PredicateType<TArg: TriggerArguments, TState: TriggerState>(
     final override val typeName: String, final override val argumentType: KType
 ): PredicateTypeBase, DowncastTriggerType<TArg, TState> {
     override val tickRate: Int = 1
+    override val argDisplayNames: Map<String, String>
+        get() = super.argDisplayNames +
+            argumentType.jvmErasure.declaredMembers.map { it.name }.associateWith { it.prettify() }
 
     @Suppress("UNUSED")
     protected abstract fun test(arguments: TArg, state: TState): Boolean
