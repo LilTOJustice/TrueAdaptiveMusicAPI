85c9e0f22f6f7758f2b200c36f9ab2d26f99c11e
LilTOJustice <muianick4@gmail.com>
2026-07-21 23:07:49 -0700

    Don't include mc version in artifact id

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -90,7 +90,7 @@ sourceSets {
 publishing {
     publications {
         create<MavenPublication>("fabric") {
-            artifactId = "${project.property("mod_id")}-${project.property("minecraft_version")}-fabric"
+            artifactId = "${project.property("mod_id")}-fabric"
             from(components["java"])
         }
     }
