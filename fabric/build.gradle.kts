e7b06faca76a08c6f2d5d481100c7b4a2932fbe7
LilTOJustice <muianick4@gmail.com>
2026-07-23 02:30:17 -0700

    Fix java version

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -14,12 +14,9 @@ base {
     archivesName.set(project.property("archives_base_name") as String)
 }
 
-val targetJavaVersion = 25
+val targetJavaVersion = (project.property("java_version") as String).toInt()
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
-}
-
-repositories {
 }
 
 dependencies {
