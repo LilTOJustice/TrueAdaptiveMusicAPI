e5bc485131173c87c64143a1ecbfdf43e4c0c5de
LilTOJustice <muianick4@gmail.com>
2026-08-12 00:34:19 -0700

    1.3.7 backport to 1.20.1

--- common/src/main/kotlin/liltojustice/trueadaptivemusicapi/identifier/ItemIdentifier.kt
+++ common/src/main/kotlin/liltojustice/trueadaptivemusicapi/identifier/ItemIdentifier.kt
@@ -4,12 +4,12 @@ import net.minecraft.client.Minecraft
 import net.minecraft.core.Holder
 import net.minecraft.core.Registry
 import net.minecraft.core.registries.Registries
-import net.minecraft.resources.Identifier
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Item
 import kotlin.jvm.optionals.getOrNull
 
 @Suppress("UNUSED")
-class ItemIdentifier(id: Identifier): TypedIdentifier(id) {
+class ItemIdentifier(id: ResourceLocation): TypedIdentifier(id) {
     override fun toPrefixedLanguageKey(): String {
         return id.toLanguageKey("item")
     }
@@ -17,25 +17,23 @@ class ItemIdentifier(id: Identifier): TypedIdentifier(id) {
     fun matches(item: Holder<Item>): Boolean {
         val registry = getItemRegistry() ?: return false
 
-        return registry[id].getOrNull()?.let {
-            item.`is`(it.key())
-        } ?: registry.tags.toList().firstOrNull { it.key().location == id }?.key()?.let {
+        return registry.tagNames.toList().firstOrNull { it.location == id }?.let {
             item.`is`(it)
-        } ?: false
+        } ?: (registry[id] == item.value())
     }
 
     companion object: TypedIdentifierCompanion() {
-        override fun getRegistryIds(): List<Identifier> {
+        override fun getRegistryIds(): List<ResourceLocation> {
             val registry = getItemRegistry() ?: return emptyList()
 
-            return registry.keySet().toList() +
-                    registry.tags.map { it.key().location }.filter { it.namespace != "c" }.toList()
+            return registry.registryKeySet().toList().map { it.location() } +
+                    registry.tagNames.toList().map { it.location }.filter { it.namespace != "c" }.toList()
         }
 
         private fun getItemRegistry(): Registry<Item>? {
             return Minecraft.getInstance().level
                 ?.registryAccess()
-                ?.lookup(Registries.ITEM)
+                ?.registry(Registries.ITEM)
                 ?.getOrNull()
         }
     }
