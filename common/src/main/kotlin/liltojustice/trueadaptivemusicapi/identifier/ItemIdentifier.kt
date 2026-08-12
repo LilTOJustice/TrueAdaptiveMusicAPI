301b290707cc148fb8156e204b087fe273962468
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-08-12 00:39:17 -0700

    1.21.2: Cherry pick commit (#63)
    * 1.3.7 add item identifier
    
    * 1.3.7 backport to 1.21.2

---
+++ common/src/main/kotlin/liltojustice/trueadaptivemusicapi/identifier/ItemIdentifier.kt
@@ -0,0 +1,42 @@
+package liltojustice.trueadaptivemusicapi.identifier
+
+import net.minecraft.client.Minecraft
+import net.minecraft.core.Holder
+import net.minecraft.core.Registry
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import kotlin.jvm.optionals.getOrNull
+
+@Suppress("UNUSED")
+class ItemIdentifier(id: ResourceLocation): TypedIdentifier(id) {
+    override fun toPrefixedLanguageKey(): String {
+        return id.toLanguageKey("item")
+    }
+
+    fun matches(item: Holder<Item>): Boolean {
+        val registry = getItemRegistry() ?: return false
+
+        return registry[id].getOrNull()?.let {
+            item.`is`(it.key())
+        } ?: registry.tags.toList().firstOrNull { it.key().location == id }?.key()?.let {
+            item.`is`(it)
+        } ?: false
+    }
+
+    companion object: TypedIdentifierCompanion() {
+        override fun getRegistryIds(): List<ResourceLocation> {
+            val registry = getItemRegistry() ?: return emptyList()
+
+            return registry.keySet().toList() +
+                    registry.tags.map { it.key().location }.filter { it.namespace != "c" }.toList()
+        }
+
+        private fun getItemRegistry(): Registry<Item>? {
+            return Minecraft.getInstance().level
+                ?.registryAccess()
+                ?.lookup(Registries.ITEM)
+                ?.getOrNull()
+        }
+    }
+}
